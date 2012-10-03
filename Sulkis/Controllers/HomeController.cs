cd42de0a2264fab091dab1ebbcd24f9b69102fd8
Petri Pulkkila <petri.pulkkila@gmail.com>
2012-10-03 11:33:30 +0300

    Muutettu description-teksti

--- Sulkis/Controllers/HomeController.cs
+++ Sulkis/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Sulkis.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Tämä on Peten tekemä sulkapallotulosten seurantasofta.";
 
             return View();
         }
