ae8a074e44ac97a4aa190368da711c2a304e0796
Petri Pulkkila <petri.pulkkila@gmail.com>
2012-10-03 11:26:33 +0300

    Added RavenDB Client and removed login models

--- Sulkis/App_Start/AuthConfig.cs
+++ Sulkis/App_Start/AuthConfig.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Web.WebPages.OAuth;
-using Sulkis.Models;
-
 namespace Sulkis
 {
     public static class AuthConfig
